b9af6f8bf7b835923bcd3666e39538b5a05484d9
breantan <breantan@localhost>
2018-12-03 11:40:16 +0100

    Creation de la fonction nourrir

--- src/Fourmiliere/Ouvriere.java
+++ src/Fourmiliere/Ouvriere.java
@@ -1,5 +1,45 @@
 package Fourmiliere;
 
 public class Ouvriere extends Role{
+	
+	
+	
+	
+	
+	boolean nourrir(Fourmi f, Fourmiliere laFourmiliere) {
+		
+		double quantite=laFourmiliere.getQuantiteNourrirture();	
+		if(!(f.getVivant()))
+			return false;
+		
+		if (quantite>0 && f.getResteAManger()>0)
+			
+			if(quantite<=f.getResteAManger()) {
+				
+				f.manger(quantite);
+				quantite=0;
+				return true;
+				
+			} else {
+				
+				f.manger(f.getResteAManger());
+				quantite=quantite-f.getResteAManger();
+				return true;
+				
+									
+			}
+				
+	}
+	
+	void nourrir(Fourmiliere laFourmiliere){
+		
+		double quantite=laFourmiliere.getQuantiteNourrirture();
+		
+		for(int i=0;i<=laFourmiliere.listeFourmis.size()-1;i++) {
+			
+			nourrir(laFourmiliere.listeFourmis.get(i), laFourmiliere);
+				
+	}
+	
 
 }
