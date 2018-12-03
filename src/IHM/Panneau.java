a91cc22ad29c0b729cabdf78ab1522e2041e79fd
le_papre <le_papre@localhost>
2018-12-03 09:50:20 +0100

    Interface Modif 2

---
+++ src/IHM/Panneau.java
@@ -0,0 +1,14 @@
+package IHM;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Panneau extends JPanel { 
+	
+	public void paintComponent(Graphics g){
+		
+		g.fillOval(20, 20, 75, 75);
+	}               
+}
+
--- src/IHM/Simulation.java
+++ src/IHM/Simulation.java
@@ -1,18 +1,21 @@
 package IHM;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Simulation extends JFrame {
-
-	public void creerFourmiliere() {
-		
-		
-		this.setTitle("Fourmiliere");
-	    this.setSize(400, 500);
-	    this.setLocationRelativeTo(null);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+	
+	public void creerFourmiliere() { 
+		this.setTitle("Simulation Fourmiliere");
+	    this.setSize(500, 500);
+	    this.setLocationRelativeTo(null);               
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setContentPane(new Panneau());
 	    this.setVisible(true);
-		
+	    
 	}
 	
 	
