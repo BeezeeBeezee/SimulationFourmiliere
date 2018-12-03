6c4502540baf3ef2399d9614e2a19a0072d7bad9
allenoma <allenoma@localhost>
2018-12-03 12:00:14 +0100

    Modification de la classe Ouvriere

--- src/fourmiliere/Ouvriere.java
+++ src/fourmiliere/Ouvriere.java
@@ -3,44 +3,41 @@ package fourmiliere;
 public class Ouvriere extends Role{
 	
 	
-	
-	
-	
-	boolean nourrir(Fourmi f, Fourmiliere laFourmiliere) {
+	public Ouvriere() {
 		
-		double quantite=laFourmiliere.getQuantiteNourrirture();	
-		if(!(f.getVivant()))
-			return false;
-		
-		
-		if (quantite>0 && f.getResteAManger()>0)
-			
-			
-			if(quantite<=f.getResteAManger()) {
-				
-				f.manger(quantite);
-				quantite=0;
-				return true;
-				
-				
-			} else {
-				
-				f.manger(f.getResteAManger());
-				quantite=quantite-f.getResteAManger();
-				return true;
-				
-									
-			}
-				
 	}
 	
-	void nourrir(Fourmiliere laFourmiliere){
+	
+	public boolean nourrir(Fourmi f, Fourmiliere laFourmiliere) {
+		boolean resultat = false;
 		
-		double quantite=laFourmiliere.getQuantiteNourrirture();
+		double quantite=laFourmiliere.getQuantiteNourrirture();	
+		if(!(f.getVivant())) {
+			resultat = false;
+		}
+		
+		if (quantite>0 && f.getResteAManger()>0) {
+			if(quantite<=f.getResteAManger()) {
+				f.mange(quantite);
+				quantite=0;
+				resultat = true;
+			} else {
+				f.mange(f.getResteAManger());
+				quantite=quantite-f.getResteAManger();
+				resultat = true;			
+			}
+		}
+		return resultat;	
+	}
+	
+	public void nourrir(Fourmiliere laFourmiliere){
+		
+		double quantite = laFourmiliere.getQuantiteNourrirture();
 		
 		for(int i=0;i<=laFourmiliere.listeFourmis.size()-1;i++) {
 			
 			nourrir(laFourmiliere.listeFourmis.get(i), laFourmiliere);
+		}
 				
 	}
 	
