f713945c9c7d9817fdb4b833d0ebe3ac5fbcb306
allenoma <allenoma@localhost>
2018-12-03 10:09:21 +0100

    Modification de Fourmi
    Ajout de methode: new(), evolution(), isGonnaDie();

--- src/Fourmiliere/Fourmi.java
+++ src/Fourmiliere/Fourmi.java
@@ -1,15 +1,32 @@
 package Fourmiliere;
 
 public class Fourmi {
+	private int id = 0;
 	int idFourmi;
 	int age; //Jour
 	double poids; 
+	double quantiteNourritureMange;
 	
 	Sexe sexe;
 	Etat etat; //Vivant ou Mort
 	Etape etape;	
 	
 	public Fourmi(){
+		idFourmi = id;
+		id++;                                                                                                                                                                                                                      
 		age = 0;
+		poids = 0.1; //mg
+		quantiteNourritureMange = Math.random() * (0 - 1);
+		sexe = sexe.FEMELLE;
 	}
+	
+	public void evolution() {
+		
+	}
+	
+	public void isGonnaDie() {
+		
+	}
+	
+	
 }
