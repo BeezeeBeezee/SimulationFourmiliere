92456e2f5bba9adc687c09f478930b7aa85817f2
le_papre <le_papre@localhost>
2018-12-04 09:13:28 +0100

    Nymphe

---
+++ src/fourmiliere/Nymphe.java
@@ -0,0 +1,26 @@
+package fourmiliere;
+
+public class Nymphe extends Etape{
+
+	public Nymphe() {
+		super();
+		this.poids = 0;
+		this.resteAManger = 0;
+	}
+	
+	@Override
+	double getPoids() {
+		return this.poids;
+	}
+
+	@Override
+	double getResteAManger() {
+		return this.resteAManger;
+	}
+
+	@Override
+	boolean mange(double aEteMange) {
+		this.resteAManger -= aEteMange;		
+		return this.resteAManger > 0;	
+	}
+}
