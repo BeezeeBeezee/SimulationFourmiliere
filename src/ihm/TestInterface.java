a56372138b3e5ce34a20988dea4c26d24ed1a8f6
le_papre <le_papre@localhost>
2018-12-05 09:30:45 +0100

    Modification des déplacements et modification de l'interface graphique

--- src/ihm/TestInterface.java
+++ src/ihm/TestInterface.java
@@ -4,6 +4,7 @@ package ihm;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -11,36 +12,60 @@ import java.util.Random;
 import fourmiliere.Adulte;
 import fourmiliere.Etape;
 import fourmiliere.Fourmi;
+import fourmiliere.Fourmiliere;
 import fourmiliere.Nymphe;
 import fourmiliere.Oeuf;
 import fourmiliere.Ouvriere;
+import fourmiliere.Terrain;
 
 public class TestInterface {
 
 	public static void main(String[] args) {
-		int x = 20;
-		int y = 20;
+
 		World jc = new World("Simulation Fourmilière");
 		jc.setBackground(Color.WHITE);
 		jc.setPreferredSize(new Dimension(800, 600));
 		
 		Dimension dimFourmiliere = new Dimension(100, 100);
-		Dimension dimFourmi = new Dimension(5, 5);
+		Dimension dimFourmi = new Dimension(15, 15);
 		
-		jc.add(new Rect(Color.RED, new Point(300, 200), dimFourmiliere));
-		jc.add(new Oval(Color.BLACK, new Point(300, 200), dimFourmiliere));
-			
-		for (int i = 0; i < 5; i++) {
-			dimFourmi = new Dimension(15, 15);
-			jc.add(new Oval(Color.BLUE, new Point(100, 100), dimFourmi));
+		//jc.add(new Rect(Color.RED, new Point(300, 200), dimFourmiliere));
+		//jc.add(new Oval(Color.BLACK, new Point(300, 200), dimFourmiliere));
+
+		// GESTION DE LA FOURMILIERE
+		Fourmiliere laFourmiliere = new Fourmiliere();
+		Terrain leTerrain = new Terrain();	
+		
+		/*Fourmi fourmi1 = new Fourmi();
+		Fourmi fourmi2 = new Fourmi();
+		laFourmiliere.addListe(fourmi1);
+		laFourmiliere.addListe(fourmi2);
+		jc.add(new Oval(Color.BLUE, new Point(fourmi1.getX(), fourmi1.getY()), dimFourmi));
+		jc.add(new Oval(Color.BLUE, new Point(fourmi2.getX(), fourmi2.getY()), dimFourmi));*/
+		
+		for(int i=0; i < 10; i++) {
+			laFourmiliere.pond();
+			jc.add(new Oval(Color.BLUE, new Point(10, 10), dimFourmi));
 		}
+		
+
+		int val;
 		jc.open();
 		while (true) {
-			x=20;
-			y=20;
+			// AJOUT DANS LA COLLECTION
+			val = 0;
+			
+			for (int i=0; i < 10; i++) {
+				leTerrain.seDeplacer(laFourmiliere.getListe().get(i));
+			}
+			
+			//System.out.println(fourmi1.getX() + " - " + fourmi1.getY());
+			
+			
 			List<Morph> drawables = jc.contents();
 			for (Iterator<Morph> iter = drawables.iterator(); iter.hasNext();) {
-				iter.next().setPosition(new Point(x + 100, y + 100));
+				iter.next().setPosition(new Point(laFourmiliere.getListe().get(val).getX(), laFourmiliere.getListe().get(val).getY()));
+				val++;
 			}
 			
 			try {
