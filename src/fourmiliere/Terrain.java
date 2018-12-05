8899f8d9660e11ed2233718b1af53d357883fbb7
le_papre <le_papre@localhost>
2018-12-05 15:16:18 +0100

    Amelioration de Terrain en lien avec les Pheromones

--- src/fourmiliere/Terrain.java
+++ src/fourmiliere/Terrain.java
@@ -5,33 +5,55 @@ import java.util.List;
 
 
 public class Terrain {
-
-	private Case[][] lesCases;
 	
 	private List<Pheromone> lesPheromones;
+	private int pourcentageGauche;
+	private int pourcentageDroite;
+	private int pourcentageHaut;
+	private int pourcentageBas;
 	
 	public Terrain() {
 		lesPheromones = new ArrayList<Pheromone>();
+		this.pourcentageGauche = 25;
+		this.pourcentageDroite = 25;
+		this.pourcentageHaut = 25;
+		this.pourcentageBas = 25;
 	}
 	
+	/**
+	 * Méthode qui ajoute un Pheromone dans la collection de Pheromone
+	 **/
 	public void ajouterPheromone(Pheromone pheromone) {
 		lesPheromones.add(pheromone);
 	}
 	
+	/**
+	 * Méthode qui supprime un Pheromone présent dans la collection de Pheromone
+	 **/
 	public void supprimerPheromone(Pheromone pheromone) {
 		lesPheromones.remove(pheromone);	
 	}
 	
+	/**
+	 * Méthode qui vérifie les Pheromones afin d'ajuster les pourcentages de direction
+	 **/
+	public void verifierPheromones(Fourmi fourmi) {
+		
+		for (int i = 0; i < lesPheromones.size(); i++) {
+			
+		}
+	}
+	
+	/**
+	 * Méthode qui permet à la fourmi de se mouvoir
+	 **/
 	public void seDeplacer(Fourmi fourmi) {
 		int probaDeplacement =  (int) (Math.random() * 99 + 1);
 		int nouvPos;
 		
-		if ((probaDeplacement > 0) && (probaDeplacement < 25)) { //Deplacement à gauche
-			
-			for (int i = 0; i < lesPheromones.size(); i++) {
-				
-			}
-			
+		verifierPheromones(fourmi);
+		
+		if ((probaDeplacement > 0) && (probaDeplacement < this.pourcentageGauche)) { //Deplacement à gauche		
 			nouvPos = fourmi.getX() - 1;
 			if (nouvPos < 0) {
 				nouvPos = 0;
@@ -39,7 +61,7 @@ public class Terrain {
 			fourmi.setX(nouvPos);
 		}
 		
-		if ((probaDeplacement > 24) && (probaDeplacement < 50)) { //Deplacement à droite
+		else if ((probaDeplacement > this.pourcentageGauche - 1 ) && (probaDeplacement < this.pourcentageGauche + this.pourcentageDroite)) { //Deplacement à droite
 			nouvPos = fourmi.getX() + 1;
 			if (nouvPos > 600) {
 				nouvPos = 600;
@@ -47,15 +69,14 @@ public class Terrain {
 			fourmi.setX(nouvPos);
 		}
 		
-		if ((probaDeplacement > 49) && (probaDeplacement < 75)) { //Deplacement vers le bas
+		else if ((probaDeplacement > this.pourcentageGauche + this.pourcentageDroite -1 ) && (probaDeplacement < this.pourcentageGauche + this.pourcentageDroite + this.pourcentageBas)) { //Deplacement vers le bas
 			nouvPos = fourmi.getY() + 1;
 			if (nouvPos > 600) {
 				nouvPos = 600;
 			}
 			fourmi.setY(nouvPos);
-		}
-		
-		if ((probaDeplacement > 74) && (probaDeplacement < 100)) { //Deplacement vers le haut
+		}		
+		else { //Deplacement vers le haut
 			nouvPos = fourmi.getY() - 1;
 			if (nouvPos < 0) {
 				nouvPos = 0;
