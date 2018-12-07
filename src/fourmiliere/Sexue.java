8b585fb71726e3f7ea789c9bafd8d2af05cb3a4f
allenoma <allenoma@localhost>
2018-12-07 09:39:55 +0100

    Mise a jour de la classe Sexue et de la methode travaille()

--- src/fourmiliere/Sexue.java
+++ src/fourmiliere/Sexue.java
@@ -10,5 +10,9 @@ public class Sexue extends Role{
 		return "[ Sexue ]";
 	}
 	
-	public void travaille() {}
+	public void travaille() {
+		if( (this.adulte.fourmi.fourmiliere.temps.lireLeTemps()%365) == 300) {
+			this.adulte.fourmi.fourmiliere.remove(this.adulte.fourmi);
+		}
+	}
 }
